c29d1f05966c3665f5bcc96ea0d3269035eb34ed
lingxiaomeng <574243996@qq.com>
2021-06-05 01:25:18 -0700

    update cal

--- src/cal.py
+++ src/cal.py
@@ -1,16 +1,13 @@
 from math import cos, sin
-
-ps3_radians = 0.5759972
-x3 = 2.5153 + 0.5 * cos(ps3_radians) - 0.5 * sin(ps3_radians)
-y3 = -0.448662 + 0.5 * sin(ps3_radians) + 0.5 * cos(ps3_radians)
-
 ps2_radians = 0.559347
 x2 = -0.03884 + 0.5 * cos(ps2_radians) + 0.5 * sin(ps2_radians)
 y2 = 4.08117 + 0.5 * sin(ps2_radians) - 0.5 * cos(ps2_radians)
-
+ps3_radians = 0.5759972
+x3 = 2.5153 + 0.5 * cos(ps3_radians) - 0.5 * sin(ps3_radians)
+y3 = -0.448662 + 0.5 * sin(ps3_radians) + 0.5 * cos(ps3_radians)
 ps4_radians = 0.5759972
-x4 = 6.87578 - 0.5 * cos(ps2_radians) - 0.5 * sin(ps2_radians)
-y4 = 2.19974 - 0.5 * sin(ps2_radians) + 0.5 * cos(ps2_radians)
+x4 = 6.87578 - 0.5 * cos(ps4_radians) - 0.5 * sin(ps4_radians)
+y4 = 2.19974 - 0.5 * sin(ps4_radians) + 0.5 * cos(ps4_radians)
 
 print (x2)
 print (y2)
